16b34aa46e65acf9e2a175c04d79f8e36fdc68f7
zehnpaard <zehnpaard@googlemail.com>
2014-01-03 21:13:39 +0000

    Exploration of Int, Long and Float Types
    Code and output that examines the running costs of various mathematical
    operations on the built-in Int, Long and Float types

--- blog_utils.py
+++ blog_utils.py
@@ -7,38 +7,80 @@ to write up on my blog
 
 import timeit as timeit_
 
-def timeit(code_string, setup='pass'):
+
+
+def timeit(code_string, setup='pass', output='print'):
     """ Implementation of timeit that replicates behaviour of the
     'python -mtimeit' command, to make it available in the interactive 
     prompt and saved modules. 
     """
+    assert output in ('print', 'return')
+
+
     USEC_PER_SEC = 10**6
 
 
+    """
+    Get the 'best of three' average per loop in microseconds
+    The number of loops starts with 1, and goes up by an order
+    of magnitude until the time taken to compute all the loops 
+    3 times becomes greater than 200 milliseconds
+    """
     number_of_loops = 1
     total_iteration_time = 0
+
     while total_iteration_time < 0.2:
         number_of_loops *= 10
-
         time_result_list = timeit_.repeat(stmt=code_string,
                 setup=setup, number=number_of_loops)
-
         total_iteration_time = sum(time_result_list)
 
-        best_time_per_loop = (min(time_result_list) * USEC_PER_SEC 
-                                                    * 1./number_of_loops)
+    best_time_per_loop = (min(time_result_list) * USEC_PER_SEC 
+                                                * 1./number_of_loops)
+    representation = 'us'
 
+    """
+    Update time and the string holding representation if 
+    the time is greater than 1 millisecond or smaller 
+    than 1000 nanoseconds
+    """
     if best_time_per_loop  >= 1000:
         best_time_per_loop  /= 1000
         representation = 'ms'
     elif best_time_per_loop  < 1:
         best_time_per_loop  *= 1000
         representation = 'ns'
-    else:
-        representation = 'us'
 
-    preformat_string = '{0:10d} loops, best of 3: {1:5.1f} {2} per loop' 
-    print preformat_string.format(number_of_loops,
-                                  best_time_per_loop,
-                                  representation)
 
+    # Print or return tuple based on the 'output' flag
+    if output == 'print':
+        preformat_string = '{0:10d} loops, best of 3: {1:5.1f} {2} per loop' 
+        print preformat_string.format(number_of_loops,
+                                      best_time_per_loop,
+                                      representation)
+    elif output == 'return':
+        return (number_of_loops, best_time_per_loop, representation)
+
+
+def print_timeit(code_strings):
+    """ Take an iterable with code strings.  For each code string, print 
+    the code string and the timeit output, with some embellishments
+    """
+    print
+    for code in code_strings:
+        print code
+        print '-'*50
+        timeit(code)
+        print '-'*50
+        print
+        
+
+def print_timeit_table(code_strings):
+    """ Take an iterable with code strings. Print a summary table that 
+    displays the code and timeit outputs.
+    """
+    print '{0:40}:{1:>7}'.format('Code', 'Time Taken')
+    print '-'*51
+    for code in code_strings:
+        loops, time, representation = timeit(code, output='return')
+        print '{0:40}:{1:7.1f}{2:>3}'.format(code, time, representation)
---
+++ timeit_ints_floats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Tests execution of various operations on integers and floats
+"""
+
+from blog_utils import timeit, print_timeit, print_timeit_table
+
+
+
+def create_code_strings():
+    """ Create list of text strings that are the snippets of code
+    to be run against timeit
+    """
+
+    base_string = "{0} {1} {2}"
+    
+    numbers = (1, 8, 13, int(2**31-1), int(2**31))
+    float_nums = [float(n) for n in numbers]
+    operators = ('+', '-', '*', '/', '%')
+    code_strings = [base_string.format(n, op, m) for n in numbers
+                                                 for op in operators
+                                                 for m in numbers]
+
+    code_strings += [base_string.format(n, op, m) for n in float_nums
+                                                  for op in operators
+                                                  for m in float_nums]
+
+    code_strings += [base_string.format(n, op, m) for n in float_nums
+                                                  for op in operators
+                                                  for m in numbers]
+
+    exponent_strings = ('{0}**{1}', 'pow({0},{1})', '{0}**-{1}', 'pow({0},-{1})')
+    code_strings += [exp_string.format(n, m) for exp_string in exponent_strings
+                                             for n in numbers
+                                             for m in numbers[:-2]]
+
+    code_strings += ['float({0})'.format(n) for n in numbers]
+    code_strings += ['{0}*1.'.format(n) for n in numbers]
+    code_strings += ['int({0})'.format(n) for n in float_nums]
+
+    return code_strings
+
+
+def main():
+
+    code_strings = create_code_strings()
+
+    print_timeit_table(code_strings)
+
+
+if __name__ == '__main__':
+    main()
+
+
